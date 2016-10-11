d608c68fd8a988ce2a030b26362ca11301c94dc6
Naimdjon Takhirov <naimdjon@gmail.com>
2016-10-11 03:47:39 +0200

    data for last names

--- exercise7_hadoop_custom_reader/prep.sh
+++ exercise7_hadoop_custom_reader/prep.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cat firstname.dat | cut -f 2 >> firsNames.txt
 cat firstname.dat | cut -f 4 >> firsNames.txt
 sed -e ':a' -e 'N' -e 'ba' -e 's/\n/,/g' firstNames.txt
@@ -5,5 +6,8 @@ echo "Firstnames:"
 echo "===================================="
 cat firstNames.txt
 sed -e ':a' -e 'N' -e 'ba' -e 's/\n/,/g' firstNames.txt
+cp last.dat lastNames.txt
+sed -e ':a' -e 'N' -e 'ba' -e 's/\n/,/g' lastNames.txt
+
 
 
